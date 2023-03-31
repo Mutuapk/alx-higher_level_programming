9151588069635dacf797ee5d4e1de834b2f3a9aa
Peter Mutua <mutuapeterk@gmail.com>
2023-03-31 05:08:00 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in URL, sends a request, displays size of the body of response
-curl -sI "$@" | grep -i Content-Length | awk '{print $2}'
+curl -sI "URL" | grep -i "Content-Length" | grep -Eo "[0-9]+"
 

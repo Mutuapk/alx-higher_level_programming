9c062d08cb2dc0d9ca595db5b5413b4c32c6fe9a
Peter Mutua <mutuapeterk@gmail.com>
2023-04-03 05:24:02 -0700

    0. cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in URL, sends a request, displays size of the body of response
+# send a request to an URL with curl, and displays the size of the body of the response
 curl -s "$1" | wc -c
